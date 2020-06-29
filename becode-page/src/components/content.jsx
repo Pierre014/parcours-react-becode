4d3bfccb5558ae73bd4e5b4c0c4b28f2dbe58144
Pierre014 <pierrelroand1406@gmail.com>
2020-06-29 15:52:45 +0200

    color theme

--- becode-page/src/components/content.jsx
+++ becode-page/src/components/content.jsx
@@ -5,32 +5,32 @@ const Content = () => {
 
  return (<div className="row d-flex justify-content-center">
         <div className="col-12 col-sm-2 col-lg-2">
-            <h5>Apprendre à aprendre</h5>
-            <p>Chez BeCode on n'apprend <br/> pas un langage en <br/> particulier, on apprend à 
+            <h6>Apprendre à aprendre</h6>
+            <p className="parag">Chez BeCode on n'apprend <br/> pas un langage en <br/> particulier, on apprend à 
                 <br/> apprendre
             </p>
         </div>
         <div className="col-12 col-sm-2 col-lg-2">
-            <h5>Pédagogie active</h5>
-            <p>Pas de cours magistraux <br/> embétant dont on ne <br/> retient rien. ici la 
+            <h6>Pédagogie active</h6>
+            <p className="parag">Pas de cours magistraux <br/> embétant dont on ne <br/> retient rien. ici la 
                 <br/> est active
             </p>
         </div>
         <div className="col-12 col-sm-2 col-lg-2">
-            <h5>Ouvert à tou.t.e.s</h5>
-            <p>Votre genre, votre religion, <br/> votre couleur de peau, votre <br/> votre âge,
+            <h6>Ouvert à tou.t.e.s</h6>
+            <p className="parag">Votre genre, votre religion, <br/> votre couleur de peau, votre <br/> votre âge,
                 vos diplômes, ON S'EN <br/> FOUT
             </p>
         </div>
         <div className="col-12 col-sm-2 col-lg-2">
-            <h5>gratuit</h5>
-            <p>La formation est <br/> entièrement gratuite. Si vous <br/> n'avez pas de PC, on vous
+            <h6>gratuit</h6>
+            <p className="parag">La formation est <br/> entièrement gratuite. Si vous <br/> n'avez pas de PC, on vous
                 en <br/> prête un.
             </p>
         </div>
         <div className="col-12 col-sm-2 col-lg-2">
-            <h5>intensif</h5>
-            <p>Du lundi au vendredi, de <br/> 9h00 à 17h00, vous <br/> apprendrez comme si vous
+            <h6>intensif</h6>
+            <p className="parag">Du lundi au vendredi, de <br/> 9h00 à 17h00, vous apprendrez comme si vous
                 <br/> étiez en entreprise.
             </p>
         </div>
--- becode-page/src/components/intro.jsx
+++ becode-page/src/components/intro.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Intro = () => {
 
-    return <p>
+    return <p id='presentation'>
                 Bienvenue à BeCode, la nouvelle formation en horaire de <br/>jour pour apprendre les 
                 technologie numérique <br/> gratuitement.
            </p>
